6ac87b37f0fb4cd0dd9c7ab94621d8ca79487b7f
Usuario Local <Usuario Local>
2017-03-16 16:45:33 +0100

    Lab2

--- DSI_P1/DSI_P1/Source.cpp
+++ DSI_P1/DSI_P1/Source.cpp
@@ -5,20 +5,141 @@
 
 #include "Header.h"
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-RECT  a;
+#include <windows.h>
+#include <d2d1.h>
+#pragma comment(lib, "d2d1")
+
+
+template <class T> void SafeRelease(T **ppT)
+{
+	if (*ppT)
+	{
+		(*ppT)->Release();
+		*ppT = NULL;
+	}
+}
+
 class MainWindow : public BaseWindow<MainWindow>
 {
+	ID2D1Factory            *pFactory;
+	ID2D1HwndRenderTarget   *pRenderTarget;
+	ID2D1SolidColorBrush    *pBrush;
+	D2D1_ELLIPSE            ellipse;
+
+	void    CalculateLayout();
+	HRESULT CreateGraphicsResources();
+	void    DiscardGraphicsResources();
+	void    OnPaint();
+	void    Resize();
+	void	DrawClockHand(float fHandLength, float fAngle, float fStrokeWidth);
+	void	RenderScene();
+
 public:
-	PCWSTR  ClassName() const { return L"Sample Window Class"; }
+
+	MainWindow() : pFactory(NULL), pRenderTarget(NULL), pBrush(NULL)
+	{
+	}
+
+	PCWSTR  ClassName() const { return L"Circle Window Class"; }
 	LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
 };
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow)
+// Recalculate drawing layout when the size of the window changes.
+
+void MainWindow::CalculateLayout()
+{
+	if (pRenderTarget != NULL)
+	{
+		D2D1_SIZE_F size = pRenderTarget->GetSize();
+		const float x = size.width / 2;
+		const float y = size.height / 2;
+		const float radius = min(x, y);
+		ellipse = D2D1::Ellipse(D2D1::Point2F(x, y), radius, radius);
+	}
+}
+
+HRESULT MainWindow::CreateGraphicsResources()
+{
+	HRESULT hr = S_OK;
+	if (pRenderTarget == NULL)
+	{
+		RECT rc;
+		GetClientRect(m_hwnd, &rc);
+
+		D2D1_SIZE_U size = D2D1::SizeU(rc.right, rc.bottom);
+
+		hr = pFactory->CreateHwndRenderTarget(
+			D2D1::RenderTargetProperties(),
+			D2D1::HwndRenderTargetProperties(m_hwnd, size),
+			&pRenderTarget);
+
+		if (SUCCEEDED(hr))
+		{
+			const D2D1_COLOR_F color = D2D1::ColorF(1.0f, 1.0f, 0);
+			hr = pRenderTarget->CreateSolidColorBrush(color, &pBrush);
+
+			if (SUCCEEDED(hr))
+			{
+				CalculateLayout();
+			}
+		}
+	}
+	return hr;
+}
+
+void MainWindow::DiscardGraphicsResources()
+{
+	SafeRelease(&pRenderTarget);
+	SafeRelease(&pBrush);
+}
+
+void MainWindow::OnPaint()
+{
+	HRESULT hr = CreateGraphicsResources();
+	if (SUCCEEDED(hr))
+	{
+		PAINTSTRUCT ps;
+		BeginPaint(m_hwnd, &ps);
+
+		pRenderTarget->BeginDraw();
+	
+
+		pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::SkyBlue));
+		pRenderTarget->FillEllipse(ellipse, pBrush);
+		RenderScene();
+
+
+
+
+		hr = pRenderTarget->EndDraw();
+		if (FAILED(hr) || hr == D2DERR_RECREATE_TARGET)
+		{
+			DiscardGraphicsResources();
+		}
+		EndPaint(m_hwnd, &ps);
+	}
+}
+
+void MainWindow::Resize()
+{
+	if (pRenderTarget != NULL)
+	{
+		RECT rc;
+		GetClientRect(m_hwnd, &rc);
+
+		D2D1_SIZE_U size = D2D1::SizeU(rc.right, rc.bottom);
+
+		pRenderTarget->Resize(size);
+		CalculateLayout();
+		InvalidateRect(m_hwnd, NULL, FALSE);
+	}
+}
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
 {
 	MainWindow win;
 
-	if (!win.Create(L"Learn to Program Windows", WS_OVERLAPPEDWINDOW))
+	if (!win.Create(L"Circle", WS_OVERLAPPEDWINDOW))
 	{
 		return 0;
 	}
@@ -41,37 +162,85 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
-	case WM_DESTROY:
-		PostQuitMessage(0);
+	case WM_CREATE:
+		if (FAILED(D2D1CreateFactory(
+			D2D1_FACTORY_TYPE_SINGLE_THREADED, &pFactory)))
+		{
+			return -1;  // Fail CreateWindowEx.
+		}
+		SetTimer(m_hwnd, // handle to main window
+			0, // timer identifier
+			1000, //1-second interval
+			NULL); // timer callback
 		return 0;
-	case WM_CLOSE:
-		if (MessageBox(BaseWindow::m_hwnd, L"Really quit?", L"My application", MB_OKCANCEL) == IDOK)
-		{
-			DestroyWindow(BaseWindow::m_hwnd);
-		}
-	case WM_LBUTTONDOWN:
-		if (MessageBox(BaseWindow::m_hwnd, L"Really quit?", L"My application", MB_OKCANCEL) == IDOK)
-		{
-			DestroyWindow(BaseWindow::m_hwnd);
-		}
+
+	case WM_DESTROY:
+		DiscardGraphicsResources();
+		SafeRelease(&pFactory);
+		PostQuitMessage(0);
+		KillTimer(m_hwnd, 0);
+		return 0;
+
+	case WM_TIMER: // process the 1-second timer
+		PostMessage(m_hwnd, WM_PAINT, NULL, NULL);
+		return 0;
+
 	case WM_PAINT:
-	{
-		PAINTSTRUCT ps;
-		HDC hdc = BeginPaint(BaseWindow::m_hwnd, &ps);
+		OnPaint();
+		return 0;
 
-		a.left = 600; //x 1º
-		a.top = 150; // y1º
-		a.right = 650; // x 2º
-		a.bottom = 300;// y 2º
-		FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-		FillRect(hdc, &a, (HBRUSH)(COLOR_3DLIGHT));
 
-		EndPaint(BaseWindow::m_hwnd, &ps);
+	case WM_SIZE:
+		Resize();
+		return 0;
 	}
-	
-
-	default:
-		return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
-	}
-	return TRUE;
+	return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
 }
+
+void MainWindow::DrawClockHand(float fHandLength, float fAngle, float fStrokeWidth)
+{
+	pRenderTarget->SetTransform(
+		D2D1::Matrix3x2F::Rotation(fAngle, ellipse.point)
+		);
+
+	// endPoint defines one end of the hand.
+	D2D_POINT_2F endPoint = D2D1::Point2F(
+		ellipse.point.x,
+		ellipse.point.y - (ellipse.radiusY * fHandLength)
+		);
+
+	// Draw a line from the center of the ellipse to endPoint.
+	pRenderTarget->DrawLine(
+		ellipse.point, endPoint, pBrush, fStrokeWidth);
+}
+void MainWindow::RenderScene()
+{
+	pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::SkyBlue));
+
+	pRenderTarget->FillEllipse(ellipse, pBrush);
+	pRenderTarget->DrawEllipse(ellipse, pBrush);
+
+
+	// Draw hands
+	SYSTEMTIME time;
+	GetLocalTime(&time);
+
+	// 60 minutes = 30 degrees, 1 minute = 0.5 degree
+	const float fHourAngle = (360.0f / 12) * (time.wHour) + (time.wMinute * 0.5f);
+	const float fMinuteAngle = (360.0f / 60) * (time.wMinute);
+	const float fSecondAngle = (360.0f / 60) * time.wSecond;
+
+	const D2D1_COLOR_F prev = pBrush->GetColor();
+	const D2D1_COLOR_F color = D2D1::ColorF(0, 0, 0);
+	pRenderTarget->CreateSolidColorBrush(color, &pBrush);
+
+	DrawClockHand(0.6f, fHourAngle, 6);
+	DrawClockHand(0.85f, fMinuteAngle, 4);
+	DrawClockHand(0.9f, fSecondAngle, 2);
+
+	pRenderTarget->CreateSolidColorBrush(prev, &pBrush);
+
+	// Restore the identity transformation.
+	pRenderTarget->SetTransform(D2D1::Matrix3x2F::Identity());
+}
+
